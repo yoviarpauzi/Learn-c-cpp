b5b05afc4353ba77644e2c94f48e239ce1d2182c
yoviarpauzi <yoviarpauzi99@gmail.com>
2022-05-01 20:38:08 +0700

    add new commit

--- linked-list/single-linked-list.cpp
+++ linked-list/single-linked-list.cpp
@@ -1,68 +1,50 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int a = 0;
+int a = 1;
 struct Teman{
     string nama;
     int umur;
     Teman *next;
-}*head, *tail, *newnode, *cur;
-// Membuat node baru
+}*head, *tail, *newnode, *delnode, *cur;
 void newNode(string nama, int umur){
     head = new Teman();
     head->nama = nama;
     head->umur = umur;
     tail = head;
+    head->next = NULL;
     a++;
 }
-// Menambahkan node di awal
-void addFirst(string nama, int umur){
-    newnode = new Teman();
-    newnode->nama = nama;
-    newnode->umur = umur;
-    newnode->next = head;
-    head = newnode;
-    a++;
-}
-// Menambahkan node di akhir
 void addLast(string nama, int umur){
     newnode = new Teman();
     newnode->nama = nama;
     newnode->umur = umur;
     tail->next = newnode;
     tail = newnode;
+    tail->next = NULL;
     a++;
 }
-// Menambahkan node di tengah
-void addMiddle(string nama, int umur){
+void delLast(){
+    delnode = tail;
     cur = head;
-    int b = (a / 2) + 1;
-    int posisi = 1;
-    while(posisi < b){
+    while(cur->next != tail){
         cur = cur->next;
-        posisi++;
     }
-    newnode = new Teman();
-    newnode->nama = nama;
-    newnode->umur = umur;
-    newnode->next = cur->next;
-    cur->next = newnode;
-    a++;
+    tail = cur;
+    tail->next = NULL;
+    delete delnode;
 }
-//Prosedur untuk menampilkan node
 void printLinkedList(){
-    while(head != NULL){
-        cout << "Nama : " << head->nama << endl;
-        cout << "Umur : " << head->umur << endl;
-        head = head->next;
+    cur = head;
+    while(cur != NULL){
+        cout << "Nama : " << cur->nama << endl;
+        cout << "Umur : " << cur->umur << endl;
+        cur = cur->next;
     }
 }
-//Program utama
 int main(){
-    newNode("Afrizal", 20);
-    addFirst("Misbahul Anam", 21);
-    addLast("Maulidani Rianturi", 19);
-    addMiddle("Rizki Dinihari", 22);
-    addMiddle("Deni", 22);
+    newNode("Yoviar pauzi", 19);
+    addLast("Misbahul Anam", 21);
+    delLast();
     printLinkedList();
     return 0;
 }
