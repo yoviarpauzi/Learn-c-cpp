7fbd459bdf8709c0c0432dbc64477f0d07b6c7ab
yoviarpauzi <yoviarpauzi99@gmail.com>
2022-05-02 22:31:32 +0700

    add new commit

--- mean, mode, median/statistika.cpp
+++ mean, mode, median/statistika.cpp
@@ -1,26 +1,44 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 void Modus(int a[], int n)
 {
-	int b[n];
-	int max = *max_element(a, a + n);
-	int t = max + 1;
-	int count[t];
-	for (int i = 0; i < t; i++)
-		count[i] = 0;
-	for (int i = 0; i < n; i++)
-		count[a[i]]++;
-	int mode = 0;
-	int k = count[0];
-	for (int i = 1; i < t; i++) {
-		if (count[i] > k) {
-			k = count[i];
-			mode = i;
-		}
-	}
-	cout << "Modus : " << mode << endl;
+	int b[n], i, j, sum = 0, t, k = 0, c = 1, max = 0, mode;
+	 for (i = 0; i < n - 1; i++)
+    {
+        mode = 0;
+        for (j = i + 1; j < n; j++)
+        {
+            if (a[i] == a[j]) {
+                mode++;
+            }
+        }
+        if ((mode > max) && (mode != 0)) {
+            k = 0;
+            max = mode;
+            b[k] = a[i];
+            k++;
+        }
+        else if (mode == max) {
+            b[k] = a[i];
+            k++;
+        }
+    }
+    for (i = 0; i < n; i++)
+    {
+        if (a[i] == b[i]) 
+            c++;
+        }
+        if (c == n)
+            printf("\nThere is no mode");
+        else
+        {
+            printf("\nMode\t= ");
+            for (i = 0; i < k; i++)
+                printf("%d ",b[i]);
+        }
+        printf("\n");
 }
-double Mean(int a[], int n)
+void Mean(int a[], int n)
 {
     int sum = 0;
     for (int i = 0; i < n; i++)
@@ -28,13 +46,23 @@ double Mean(int a[], int n)
   
     cout << "Mean : " << (double)sum / (double)n << endl;
 }
-double Median(int a[], int n)
+void Median(int a[], int n)
 {
-    sort(a, a + n);
-    if (n % 2 != 0)
+	int temp = 0;
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n - i - 1; j++){
+			if(a[j] > a[j+1]){
+				temp = a[j];
+				a[j] = a[j+1];
+				a[j+1] = temp;
+			}
+		}
+	}
+	if (n % 2 != 0){
         cout << "Median : " << (double)a[n / 2] << endl;
-
+	}else{
     cout << "Median : " << (double)(a[(n - 1) / 2] + a[n / 2]) / 2.0 << endl;
+	}
 }
 int main(){
 	int a[] = {80, 91, 61, 72, 97, 88, 81, 70, 74, 
