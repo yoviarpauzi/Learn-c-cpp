bc78e544f30eec6ca1ebe9ebebf1268d6e9cf2b3
yoviarpauzi <yoviarpauzi99@gmail.com>
2022-05-31 16:14:33 +0700

    add new commit

---
+++ manipulasi string/konversi-string-to-int.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+string ConvertStringToInt(int a){
+    ostringstream convert;
+    convert << a;
+    string c = convert.str();
+    reverse(c.begin(), c.end());
+    string hasil;
+    for(int i = 0; i < c.length(); i++){
+        if((i + 1) % 3 == 0 && i != c.length() - 1){
+            hasil += c[i];
+            hasil += ".";
+        }else{
+            hasil += c[i];
+        }
+    }
+    reverse(hasil.begin(), hasil.end());
+    return hasil;
+}
+int main(){
+    int diamond, harga = 160;
+    cout << "Inputkan banyak diamond : ";
+    cin >> diamond;
+    harga *= diamond;
+    cout << "Harga Diamond : " << ConvertStringToInt(harga);
+    return 0;
+}
