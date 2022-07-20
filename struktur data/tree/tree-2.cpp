e1106c2ec9217ba66278900f5355ba9ed9e95c5c
yoviarpauzi <yoviarpauzi99@gmail.com>
2022-07-20 08:09:42 +0700

    add initial commit

--- struktur data/tree/tree-2.cpp
+++ struktur data/tree/tree-2.cpp
@@ -47,6 +47,74 @@ Tree *insertRight(char label, Tree *node){
         return newnode;
     }
 }
+// clear Tree
+void clear(){
+    if(!root)
+        cout << "Root sudah kosong!" << endl;
+    else 
+        root = NULL;
+        cout << "Seluruh node telah dihapus" << endl;
+}
+// find node
+void find(Tree *node){
+    if(!root)
+        cout << "Tree belum dibuat!" << endl;
+    else
+        // Cek apakah node ada
+        if(!node)
+            cout << "Node tidak ada!" << endl;
+        else
+            cout << "Node ada pada Tree!" << endl;
+        // cek apakah node memiliki parent
+        if(!node->parent)
+            cout << "Node tidak mempunyai parent!" << endl;
+        else{
+            cout << "Parent node : " << node->parent->label << endl;
+                if(!node->parent->right || !node->parent->left){
+                    cout << "Tidak memiliki saudara" << endl;
+                }else if(node->parent->right == node && node->parent->left){
+                    cout << "Saudara : " << node->parent->left->label << endl; 
+                }else if(node->parent->left == node && node->parent->right){
+                    cout << "Saudara : " << node->parent->right->label << endl;
+                }
+        }
+        // mengecek apakah node memiliki child
+        if(!node->left&& !node->right){
+            cout << "Tidak memiliki child" << endl;
+        }else if(!node->left){
+            cout << "Anak kanan : " << node->right->label << endl; 
+        }else if(!node->right){
+            cout << "Anak kiri : " << node->left->label << endl;
+        }else{
+            cout << "Anak kanan : " << node->right->label << endl;
+            cout << "Anak kiri : " << node->left->label << endl;
+        }
+        // cek apakah node memiliki saudoara
+}
+// height tree
+int height(Tree *node){
+    if(node == NULL){
+        return 0;
+    }else{
+        int u = height(node->left);
+        int v = height(node->right);
+        if(u > v){
+            return u + 1;
+        }else{
+            return v + 1;
+        }
+    }
+}
+//size tree
+int sizeTree(Tree *node){
+    int size;
+    if(node == NULL){
+        return 0;
+    }else{
+        size = 1 + sizeTree(node->left) + sizeTree(node->right);
+    }
+    return size;
+}
 // print preorder
 void preorder(Tree *node = root){
     if(!node)
@@ -79,5 +147,7 @@ int main(){
     newTree('A');
     B = insertLeft('B', root);
     C = insertRight('C', root);
-    preorder();
+    find(B);
+    cout << height(root) << endl;
+    cout << sizeTree(root) << endl;
 }
